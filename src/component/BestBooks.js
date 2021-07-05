a486440c862d2c3e1b8b8d11881bd92bb7e4b44c
thaerbraizat <thaerbraizat13@gmail.com>
2021-07-05 15:15:35 +0300

    add

--- src/component/BestBooks.js
+++ src/component/BestBooks.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import {withAuth0} from '@auth0/auth0-react';
+import { withAuth0 } from '@auth0/auth0-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import '../BestBooks.css';
+import axios from 'axios';
+
 
 class MyFavoriteBooks extends React.Component {
+  // componentDidMount = () => {
+  //   if (this.props.auth0.isAuthenticated) {
+  //     this.props.auth0.getIdTokenClaims().then(res => {
+  //       let jwt = res.__raw;
+  //       let config = {
+  //         headers: {
+  //           "Authorization": `Bearer  ${jwt}`
+  //         },
+  //         method: 'get',
+  //         baseURL: process.env.REACT_APP_URL,
+  //         url: '/test'
+
+  //       }
+  //       axios(config).then(response => {
+  //         console.log(response.data);
+  //       }).catch(err => { console.log(err); })
+  //     }).catch(err=> console.log(err));
+  //   }
+
+  // }
   render() {
-    return(
+    return (
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
