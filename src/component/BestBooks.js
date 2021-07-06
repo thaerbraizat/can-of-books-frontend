4074d51120d5efe845090a1ecfc6bd5e294d1b2d
thaerbraizat <thaerbraizat13@gmail.com>
2021-07-06 15:36:11 +0300

    fixxx

--- src/component/BestBooks.js
+++ src/component/BestBooks.js
@@ -7,34 +7,80 @@ import axios from 'axios';
 
 
 class MyFavoriteBooks extends React.Component {
-  // componentDidMount = () => {
-  //   if (this.props.auth0.isAuthenticated) {
-  //     this.props.auth0.getIdTokenClaims().then(res => {
-  //       let jwt = res.__raw;
-  //       let config = {
-  //         headers: {
-  //           "Authorization": `Bearer  ${jwt}`
-  //         },
-  //         method: 'get',
-  //         baseURL: process.env.REACT_APP_URL,
-  //         url: '/test'
+  componentDidMount = () => {
+    if (this.props.auth0.isAuthenticated) {
+      this.props.auth0.getIdTokenClaims().then(res => {
+        let jwt = res.__raw;
+        let config = {
+          headers: {
+            "Authorization": `Bearer  ${jwt}`
+          },
+          method: 'get',
+          baseURL: process.env.REACT_APP_URL,
+          url: '/test'
 
-  //       }
-  //       axios(config).then(response => {
-  //         console.log(response.data);
-  //       }).catch(err => { console.log(err); })
-  //     }).catch(err=> console.log(err));
-  //   }
+        }
+        axios(config).then(response => {
+          console.log(response.data);
+        }).catch(err => { console.log(err); })
+      }).catch(err=> console.log(err));
+    }
+  }
+//   constructor(){
+//     super();
+//     this.state={
+//       listBooks :[],
+//       email :''
+//     }
+//   }
+// componentDidMount=()=>{
+//   const url='http://localhost:8000/book?email=thaerbraizat13@gmail.com';
+//   axios.get(url).then(response=>{
+//     this.setState({
+//       listBooks : response.data
+//     })
+//     console.log(response.data);
+//   })
+// }
 
-  // }
+// getUserInput=(e)=>{
+//   this.setState({
+//     email:e.target.value
+//   })
+
+// }
+
+// sendRequest=(e)=>{
+//   e.preventDefault();
+//   const url='http://localhost:8000/book?email=thaerbraizat13@gmail.com';
+//   axios.get(url).then(response=>{
+//     this.setState({
+//       listBooks : response.data
+//     })
+//     console.log(response.data);
+//   })
+// }
+
   render() {
     return (
+      // <ol>
+      // {
+      //   this.state.listBooks.map(book => {
+      //   return <li>{book.name}</li>
+      //   })
+      // }
+      // <ol/>
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
+        {/* <from>
+          <input type='text' placeholder="email" onChange={(e)=>{this.getUserInput}}/>
+           < button onClick ={(e)=>{this.sendRequest}}>search by email</button>
+          </from> */}
       </Jumbotron>
+      
     )
   }
 }
