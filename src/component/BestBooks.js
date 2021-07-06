a5eeed29a159eff1868df9fc53b9fd4d2fd7bc6a
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-07-06 18:43:08 +0300

    add

--- src/component/BestBooks.js
+++ src/component/BestBooks.js
@@ -7,36 +7,68 @@ import axios from 'axios';
 
 
 class MyFavoriteBooks extends React.Component {
-  // componentDidMount = () => {
-  //   if (this.props.auth0.isAuthenticated) {
-  //     this.props.auth0.getIdTokenClaims().then(res => {
-  //       let jwt = res.__raw;
-  //       let config = {
-  //         headers: {
-  //           "Authorization": `Bearer  ${jwt}`
-  //         },
-  //         method: 'get',
-  //         baseURL: process.env.REACT_APP_URL,
-  //         url: '/test'
+  componentDidMount = () => {
+    if (this.props.auth0.isAuthenticated) {
+      this.props.auth0.getIdTokenClaims().then(res => {
+        let jwt = res.__raw;
+        let config = {
+          headers: {
+            "Authorization": `Bearer  ${jwt}`
+          },
+          method: 'get',
+          baseURL: process.env.REACT_APP_URL,
+          url: '/test'
 
-  //       }
-  //       axios(config).then(response => {
-  //         console.log(response.data);
-  //       }).catch(err => { console.log(err); })
-  //     }).catch(err=> console.log(err));
-  //   }
+        }
+        axios(config).then(response => {
+          console.log(response.data);
+        }).catch(err => { console.log(err); })
+      }).catch(err=> console.log(err));
+    }
 
-  // }
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      listBooks: [],
+      email: ''
+    }
+  }
+
+  getUserInput = (e) => {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  sendRequest = (e) => {
+    e.preventDefault();
+    const url = 'http://localhost:8000/book?email=nedal.r.alashqar@gmail.com';
+    axios.get(url).then(response => {
+      this.setState({
+        listBooks: response.data
+      })
+      console.log(response.data);
+    })
+  }
   render() {
     return (
       <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
+     
+          <h1>My Favorite Books</h1>
+          <p>
+            This is a collection of my favorite books
+          </p>
+          <from>
+            <input type='text' placeholder="search by email ..." onChange={(e) => { this.getUserInput }} />
+            < button onClick={(e) => { this.sendRequest }}>search by email</button>
+          </from>
+          <h2>{this.state.name}</h2>
       </Jumbotron>
     )
   }
 }
+
+
 
 export default withAuth0(MyFavoriteBooks);
